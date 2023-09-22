2c4078b5aa37c9e891673e650da144dc2e789e56
rayasF <rayas.f@gmail.com>
2023-09-22 15:33:26 -0500

    2nd draft

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
+
+
+const bookInput2 = document.querySelector('#bookInput')
+const chapterInput2 = document.querySelector('#chapterInput')
+const verseInput2 = document.querySelector('#verseInput')
+
 const button = document.querySelector('#submitButton')
-const input = document.querySelector('#bookInput').value
 
 button.addEventListener('click', async () => {
-    
+
+      
     let response = await axios.get(
-      `https://bible-api.com/john 3:16`
+      `https://bible-api.com/${bookInput}:${chapterInput}:${verseInput}`
     )
-      console.log(response.data) 
+      console.log(response) 
+
+        bookInput2.innerHTML = bookInput3
+        chapterInput2.innerHTML = chapterInput3
+        verseInput2.innerHTML = verseInput3
+        const text = (bookInput3, chapterInput3, verseInput3)
+        
+        const passage= document.getElementById("text")
+        
+        const result= (passage)
+        console.log(result)
+
+
+
   })
 
 
-  
+  //Fernando make passage = text. It should work
+
+  // https://bible-api.com/${bookInput}:${chapterInput}:${verseInput}ß
