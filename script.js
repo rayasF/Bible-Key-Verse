358a16ff74c62ac54fc21b4fe5ad46bab0163687
rayasF <rayas.f@gmail.com>
2023-09-23 14:24:15 -0500

    2nd draft

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-const bookInput2 = document.querySelector('#bookInput')
+const bookInput2 = document.querySelector('#book_Input')
 const chapterInput2 = document.querySelector('#chapterInput')
 const verseInput2 = document.querySelector('#verseInput')
 
@@ -10,7 +10,7 @@ button.addEventListener('click', async () => {
 
       
     let response = await axios.get(
-      `https://bible-api.com/${bookInput}:${chapterInput}:${verseInput}`
+      `https://bible-api.com/${bookInput}${chapterInput}:${verseInput}`
     )
       console.log(response) 
 
@@ -31,4 +31,22 @@ button.addEventListener('click', async () => {
 
   //Fernando make passage = text. It should work
 
-  // https://bible-api.com/${bookInput}:${chapterInput}:${verseInput}ß
+  // https://bible-api.com/${bookInput}:${chapterInput}:${verseInput}
+
+
+
+  // //before I made changes
+  // let response = await axios.get(
+  //   `https://bible-api.com/${bookInput}:${chapterInput}:${verseInput}`
+  // )
+  //   console.log(response) 
+
+  //     bookInput2.innerHTML = bookInput3
+  //     chapterInput2.innerHTML = chapterInput3
+  //     verseInput2.innerHTML = verseInput3
+  //     const text = (bookInput3, chapterInput3, verseInput3)
+      
+  //     const passage= document.getElementById("text")
+      
+  //     const result= (passage)
+  //     console.log(result)
