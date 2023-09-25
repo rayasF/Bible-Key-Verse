924d27c5d1f96810a474877b1d5c9f0d9a824cc2
rayasF <rayas.f@gmail.com>
2023-09-25 16:37:23 -0500

    3rd draft

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 
-
-const bookInput2 = document.querySelector('#book_Input')
-const chapterInput2 = document.querySelector('#chapterInput')
-const verseInput2 = document.querySelector('#verseInput')
-
+//getting the elements from HTML by referencing the label(class by# and saving it to location (const bookInput, chaperInput and verse Input)
+const bookInput = document.querySelector('#bookInput')
+const chapterInput = document.querySelector('#chapterInput')
+const verseInput = document.querySelector('#verseInput')
+const content= document.querySelector('#passage')
 const button = document.querySelector('#submitButton')
 
+
+//Click is an event listener. event listeners takes a string as an argument.
 button.addEventListener('click', async () => {
 
-      
-    let response = await axios.get(
-      `https://bible-api.com/${bookInput}${chapterInput}:${verseInput}`
-    )
-      console.log(response) 
-
-        bookInput2.innerHTML = bookInput3
-        chapterInput2.innerHTML = chapterInput3
-        verseInput2.innerHTML = verseInput3
-        const text = (bookInput3, chapterInput3, verseInput3)
-        
-        const passage= document.getElementById("text")
-        
-        const result= (passage)
-        console.log(result)
-
-
-
-  })
+  let response = await axios.get(
+    `https://bible-api.com/${bookInput.value} ${chapterInput.value}:${verseInput.value}`
+  )
+    //console.log(response.data) 
+    content.innerText=response.data.verses[0].text
+})
 
 
   //Fernando make passage = text. It should work
@@ -37,6 +26,7 @@ button.addEventListener('click', async () => {
 
   // //before I made changes
   // let response = await axios.get(
+    //https://bible-api.com/john 3:16
   //   `https://bible-api.com/${bookInput}:${chapterInput}:${verseInput}`
   // )
   //   console.log(response) 
